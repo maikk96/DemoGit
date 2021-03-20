81e924384d74fed6bdcf5ef7371614aa41e85fb1
Miguel Sánchez <miguelhorneros@gmail.com>
2021-03-20 11:52:02 +0100

    implementacion funcionalidad

--- src/Principal.java
+++ src/Principal.java
@@ -16,6 +16,9 @@ public class Principal {
 	 */
 	public static void main(String[] args) {
 		System.out.println("HolaMundo1");
+		
+		/* Implemntacion funcionalidad*/
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
